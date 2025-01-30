ba1c6f1b48f0b709c7b827cdd990ffff1a82df81
chonthichab8nma <chonthichaboo2@gmail.com>
2025-01-31 00:14:29 +0700

    up

--- Webaija/tripsummaryscript.js
+++ Webaija/tripsummaryscript.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const tripContent = document.getElementById("tripContent");
   const planTripButton = document.getElementById("planTripButton");
   const placeDetailButton = document.getElementById("placeDetailButton");
+  
 
   // ข้อมูลรายละเอียดทริปในรูปแบบ array
   const tripDetails = [

4424c585bac820c9a96f2bc8e2ef0305b6a90a39
pchoeiphet <phetrada.2646@gmail.com>
2025-01-31 00:12:59 +0700

    up

---
+++ Plan_detail/plan_detail.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tripContent = document.getElementById("tripContent");
+    const planTripButton = document.getElementById("planTripButton");
+    const placeDetailButton = document.getElementById("placeDetailButton");
+});

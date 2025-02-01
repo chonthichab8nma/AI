1b1413bcc31d9552a955c988c5d849c4d0641cff
Jixwayn <27746jiew@gmail.com>
2025-02-01 23:00:06 +0700

    change button home

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-document.querySelector('.next-button').addEventListener('click', function() {
+document.querySelector('.next-button').addEventListener('click', function () {
     const province = document.getElementById('province').value;
     const days = document.getElementById('days').value;
 
@@ -9,5 +9,19 @@ document.querySelector('.next-button').addEventListener('click', function() {
 
     // Redirect to the tripsummary page with query parameters
     window.location.href = url;
-  
 });
+
+function changeStyle(buttonNumber) {
+    // รีเซ็ตสถานะของปุ่มทั้งสอง
+    document.getElementById('button1').classList.remove('active');
+    document.getElementById('button2').classList.remove('active');
+
+    // เพิ่มคลาส active ให้กับปุ่มที่กด
+    if (buttonNumber === 1) {
+        document.getElementById('button1').classList.add('active');
+    } else if (buttonNumber === 2) {
+        document.getElementById('button2').classList.add('active');
+        // เมื่อกดปุ่มที่ 2 ให้ไปที่หน้า plan_detail.html
+        window.location.href = 'plan_detail.html';
+    }
+}
